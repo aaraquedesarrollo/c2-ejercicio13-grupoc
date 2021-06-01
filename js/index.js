712447d82a24f0e0dd0e7b1f7116234767b1d320
“polrb5” <polreigbroto@gmail.com>
2021-06-01 17:15:10 +0200

    Campos de las tablas rellenados

--- js/index.js
+++ js/index.js
@@ -9,8 +9,29 @@ const arrayFacturas = getFacturasTipo("ingreso");
 
 const tablaFacturas = document.querySelector(".tabla-facturas");
 
-arrayFacturas.forEach((factura) => {
+for (const {
+  numero,
+  fecha,
+  vencimiento,
+  concepto,
+  base,
+  tipoIva,
+  tipo,
+  abonada,
+} of arrayFacturas) {
   const filaDummy = document.querySelector(".factura-dummy").cloneNode(true);
   filaDummy.classList.remove("factura-dummy");
   tablaFacturas.append(filaDummy);
-});
+  const facturaNumero = filaDummy.querySelector(".factura-num");
+  facturaNumero.textContent = numero;
+  const facturaFecha = filaDummy.querySelector(".factura-fecha");
+  facturaFecha.textContent = fecha;
+  const facturaConcepto = filaDummy.querySelector(".factura-concepto");
+  facturaConcepto.textContent = concepto;
+  const facturaBase = filaDummy.querySelector(".factura-base");
+  facturaBase.textContent = base;
+  const facturaIva = filaDummy.querySelector(".factura-iva");
+  facturaIva.textContent = tipoIva;
+  const facturaTotal = filaDummy.querySelector(".factura-total");
+  facturaTotal.textContent = base + (base * tipoIva) / 100;
+}
